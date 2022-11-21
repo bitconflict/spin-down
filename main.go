1a95e765bcbfea6550e6f5b5537b31b92d61bd9a
Bit <bit.conflict@gmail.com>
2022-11-21 09:51:11 -0800

    Add logic to detect disk activity

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"os"
 	"os/exec"
+	"strconv"
+	"strings"
 )
 
 func parseArgs() (disk string, timeout int) {
@@ -15,20 +19,49 @@ func parseArgs() (disk string, timeout int) {
 	return
 }
 
-func isDiskRunningForNoReason(disk string, timeout int) (beingWasteful bool) {
+func getDiskIOTime(disk string) (iotime int, err error) {
+	file, err := os.Open("/proc/diskstats")
+	if err == nil {
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			currentLine := scanner.Text()
+			str_arr := strings.Split(currentLine, " ")
+			disk_str := str_arr[11]
+			if disk_str == disk {
+				iotime, err = strconv.Atoi(str_arr[22])
+			}
+		}
+	}
+	defer file.Close()
+	return iotime, err
+}
+func getPreviousIOTime(disk string) (iotime int) {
+	return 0
+}
 
+func isDiskRunningForNoReason(disk string, timeout int) (beingWasteful bool) {
 	beingWasteful = false
+	currentIOTime, err := getDiskIOTime(disk)
+	if err != nil {
+		fmt.Println("Couldn't read diskIOTime")
+	}
+	fmt.Printf("current io %v \n", currentIOTime)
+	previousIOTime := getPreviousIOTime(disk)
+	fmt.Printf("previous io %v \n", previousIOTime)
+	if currentIOTime == previousIOTime {
+		beingWasteful = true
+	}
 	return
 }
 
 func spinDiskDown(disk string) (err error) {
-	cmd := exec.Command("hdparm", "-y", "/dev/"+disk)
+	// cmd := exec.Command("hdparm", "-y", "/dev/"+disk)
+	cmd := exec.Command("echo", "hdparm spun down")
 	_, err = cmd.CombinedOutput()
 	return
 }
 
 func main() {
-
 	disk, timeout := parseArgs()
 	fmt.Println("Starting")
 	beingWasteful := isDiskRunningForNoReason(disk, timeout)
